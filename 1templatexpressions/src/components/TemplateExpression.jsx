3bc318b811f79b232b0fc5a424dd060598e4c518
Lucas Gomes Cruz <lucas.cruz@modalgr.io>
2024-09-17 21:26:34 -0300

    feat: uso de objeto dicionario em react

--- 1templatexpressions/src/components/TemplateExpression.jsx
+++ 1templatexpressions/src/components/TemplateExpression.jsx
@@ -1,10 +1,17 @@
 const TemplateExpression = () => {
-    const Name = "Lucas"
+    const data = {
+        name: "Lucas",
+        age: 25,
+        job: "Programmer"
+    }
+
     return (
         <div>
             <h1>Template Expressions</h1>
             <h2>{`Hello ${5+5}`}</h2>
-            <h3>{`Your name is ${Name}`}</h3>
+            <h3>{`Your name is ${data.name}`}</h3>
+            <h4>{`Your age is ${data.age}`}</h4>
+            <h5>{`Your job is ${data.job}`}</h5>
         </div>
     )
 }
