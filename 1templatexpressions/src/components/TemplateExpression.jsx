ddd9e414bfb385551b5aa8ab671177904f948cc5
Lucas Gomes Cruz <lucas.cruz@modalgr.io>
2024-09-17 21:24:50 -0300

    feat: segundo uso de javascript em template expression

--- 1templatexpressions/src/components/TemplateExpression.jsx
+++ 1templatexpressions/src/components/TemplateExpression.jsx
@@ -1,8 +1,10 @@
 const TemplateExpression = () => {
+    const Name = "Lucas"
     return (
         <div>
             <h1>Template Expressions</h1>
             <h2>{`Hello ${5+5}`}</h2>
+            <h3>{`Your name is ${Name}`}</h3>
         </div>
     )
 }
